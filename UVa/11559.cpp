50b821735e4f6e7ee529d4c287debf6798ad941a
maula <maulafaiz97@gmail.com>
2017-03-26 21:41:15 +0700

    event planning

---
+++ UVa/11559.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+  int n,b,h,w;
+  while(scanf("%d %d %d %d",&n,&b,&h,&w) != EOF){
+    int price,pay,min;
+    pay = 0;
+    min = b;
+
+    for(int ii = 0; ii < h; ii++){
+      scanf("%d",&price);
+      int kamar;
+
+      for(int jj = 0; jj < w; jj++){
+        scanf("%d",&kamar);
+        if(kamar >= n && (n*price) <= b){
+          pay = n*price;
+        }
+      }
+
+      if(pay != 0 && pay <= min){
+        min = pay;
+      }
+    }
+
+    if(pay != 0){
+      pay = min;
+      printf("%d\n",pay);
+    } else {
+      printf("stay home\n");
+    }
+  }
+  return 0;
+}
